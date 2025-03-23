e699ee3759d164179f8c9634d1afdb6ab6ba7d9d
Charles-Antoine <charto.bta@gmail.com>
2025-03-23 14:14:30 +0100

    -

--- js/focus.js
+++ js/focus.js
@@ -1,38 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNGFjZWU2Y2ZmMDBhNTNjYjE1NjE5NWNmNGEyM2M5MyIsIm5iZiI6MTczOTg4NDU0OS4zMjA5OTk5LCJzdWIiOiI2N2I0ODgwNTFlZDQ1Mjg0NTM5ZmI2NTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.xfyeXxd9pqUdNS2U8yLfciBOk5pl6hQvexrXowx6rVI'; // Remplacez par votre token d'accès
+    const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNGFjZWU2Y2ZmMDBhNTNjYjE1NjE5NWNmNGEyM2M5MyIsIm5iZiI6MTczOTg4NDU0OS4zMjA5OTk5LCJzdWIiOiI2N2I0ODgwNTFlZDQ1Mjg0NTM5ZmI2NTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.xfyeXxd9pqUdNS2U8yLfciBOk5pl6hQvexrXowx6rVI';
 
-    const fetchData = (url) => {
-        return fetch(url, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${apiKey}`,
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(response => response.json())
-            .catch(error => console.error('Erreur lors de la récupération des données:', error));
-    };
+    const fetchData = (url) => fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${apiKey}`,
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json())
+        .catch(error => console.error('Erreur lors de la récupération des données:', error));
 
     const displayMediaDetails = (mediaId, mediaType) => {
-        let mediaUrl;
-        if (mediaType === 'movie') {
-            mediaUrl = `https://api.themoviedb.org/3/movie/${mediaId}?language=fr-FR&append_to_response=credits`;
-        } else if (mediaType === 'tv') {
-            mediaUrl = `https://api.themoviedb.org/3/tv/${mediaId}?language=fr-FR&append_to_response=credits`;
-        }
+        const mediaUrl = `https://api.themoviedb.org/3/${mediaType}/${mediaId}?language=fr-FR&append_to_response=credits,videos`;
 
-        fetchData(mediaUrl)
-            .then(data => {
-                const banner = document.querySelector('.banner');
-                banner.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${data.backdrop_path})`;
+        fetchData(mediaUrl).then(data => {
+            const banner = document.querySelector('.banner');
+            banner.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${data.backdrop_path})`;
 
-                document.getElementById('focus-poster').src = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
-                document.getElementById('focus-title').innerText = data.title || data.name;
-                document.getElementById('focus-release-date').innerText = data.release_date || data.first_air_date;
-                document.getElementById('focus-overview').innerText = data.overview;
-                document.getElementById('focus-cast').innerHTML = '';
+            document.getElementById('focus-poster').src = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+            document.getElementById('focus-title').innerText = data.title || data.name;
+            document.getElementById('focus-release-date').innerText = new Date(data.release_date || data.first_air_date).toLocaleDateString('fr-FR', { day: 'numeric', month: 'short', year: 'numeric' });
+            document.getElementById('focus-overview').innerText = data.overview;
+            document.getElementById('focus-cast').innerHTML = '';
+            document.getElementById('focus-score').innerHTML = Math.round(data.vote_average * 10) + '%';
 
-                const cast = data.credits.cast.slice(0, 8);
+            const castContainer = document.getElementById('focus-cast');
+            const cast = data.credits.cast.slice(0, 8);
+
+            if (cast.length === 0) {
+                castContainer.innerHTML = '<div class="actor"><h4>Aucun acteur n\'est disponible pour ce film ou cette série.</h4></div>';
+            } else {
                 cast.forEach(actor => {
                     const actorElement = document.createElement('div');
                     actorElement.classList.add('actor');
@@ -41,12 +38,21 @@ document.addEventListener('DOMContentLoaded', () => {
                         <h4>${actor.name}</h4>
                         <span>${actor.character}</span>
                     `;
-                    document.getElementById('focus-cast').appendChild(actorElement);
+                    castContainer.appendChild(actorElement);
                 });
-            });
+            }
+
+            // Ajouter la bande-annonce
+            const trailer = data.videos.results.find(video => video.type === 'Trailer' && video.site === 'YouTube');
+            if (trailer) {
+                document.getElementById('focus-trailer').src = `https://www.youtube.com/embed/${trailer.key}`;
+            } else {
+                document.querySelector('.trailer-banner h4').style.display = 'block';
+                document.querySelector('#focus-trailer').style.display = 'none';
+            }
+        });
     };
 
-    // Récupérer les paramètres d'URL
     const urlParams = new URLSearchParams(window.location.search);
     const mediaId = urlParams.get('id');
     const mediaType = urlParams.get('type');
--- js/script.js
+++ js/script.js
@@ -1,104 +1,110 @@
 const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTgyOTUxMzViNzYwOTdiZWM5Yjk3NDJmMTQyNGIwOSIsIm5iZiI6MTczOTk1ODgyNC4zMjQsInN1YiI6IjY3YjVhYTI4ZDI4ZTg3ZTBlNWUzYjk3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qtexsioa7wxAPM9JcsrAdIi3YEFvCt3BmMVO0RW62Fs';
 
-// Fonction pour formater la date en français
-function formatDate(dateString) {
-    const options = { year: 'numeric', month: 'short', day: 'numeric' };
-    const date = new Date(dateString);
-    return date.toLocaleDateString('fr-FR', options).replace(/\//g, ' ');
+
+document.getElementById('search-bar').addEventListener('input', function() {
+    const query = this.value.trim();
+    if (query.length > 2) {
+        searchMoviesAndSeries(query);
+    } else {
+        document.getElementById('search-results').innerHTML = '';
+    }
+});
+
+function searchMoviesAndSeries(query) {
+    const url = `https://api.themoviedb.org/3/search/multi?query=${encodeURIComponent(query)}&language=fr-FR`;
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => displaySearchResults(data.results))
+        .catch(error => console.error('Erreur lors de la recherche:', error));
 }
 
-// Fonction pour mettre à jour le HTML de la section des cartes
-function updateCardSection(items, sectionId) {
+function displaySearchResults(results) {
+    const searchResultsContainer = document.getElementById('search-results');
+    searchResultsContainer.innerHTML = '';
+
+    const limitedResults = results.slice(0, 5); // Limite à 5 résultats
+
+    limitedResults.forEach(item => {
+        const title = item.title || item.name;
+        const releaseDate = item.release_date || item.first_air_date;
+        const posterPath = item.poster_path;
+        const mediaType = item.media_type;
+
+        if (posterPath && (mediaType === 'movie' || mediaType === 'tv')) {
+            const resultItem = document.createElement('div');
+            resultItem.classList.add('search-result-item');
+            resultItem.innerHTML = `
+                <a href="./pages/detail.html?id=${item.id}&type=${mediaType}" class="media-link" style="text-decoration: none;">
+                    <img src="https://image.tmdb.org/t/p/w500${posterPath}" alt="${title}">
+                    <div class="Detail-Box">
+                        <h2 class="Title">${title}</h2>
+                        <p class="Date">${formatDate(releaseDate)}</p>
+                    </div>
+                </a>
+            `;
+            searchResultsContainer.appendChild(resultItem);
+        }
+    });
+}
+
+
+
+
+
+
+
+function formatDate(dateString) {
+    return new Date(dateString).toLocaleDateString('fr-FR', { year: 'numeric', month: 'short', day: 'numeric' }).replace(/\//g, ' ');
+}
+
+function updateCardSection(items, sectionId, type) {
     const cardSection = document.querySelector(`#${sectionId} .Card-Section`);
-    cardSection.innerHTML = ''; // Réinitialiser le contenu
+    cardSection.innerHTML = '';
 
     items.forEach(item => {
         const title = item.title || item.name;
         const releaseDate = item.release_date || item.first_air_date;
         const posterPath = item.poster_path;
-        const voteAverage = item.vote_average;
-
-        const roundedVoteAverage = Math.round(voteAverage * 10);
+        const voteAverage = Math.round(item.vote_average * 10);
         const formattedDate = formatDate(releaseDate);
+        const mediaType = item.media_type !== 'movie' && item.media_type !== 'tv' ? 'tv' : item.media_type;
 
-        const cardHTML = `
-      <div class="Card-Box">
-        <a href="./pages/detail.html?id=${item.id}&type=movie" class="media-link">
-        <img src="https://image.tmdb.org/t/p/w500${posterPath}" alt="${title}">
-        <div class="Detail-Box">
-          <h2 class="Title">${title}</h2>
-          <p class="Date">${formattedDate}</p>
-        </div>
-        <svg class="Like-Box" xmlns="http://www.w3.org/2000/svg" width="63" height="63" viewBox="0 0 63 63" fill="none">
-          <circle cx="31.5" cy="31.5" r="30.5" fill="#032541" stroke="white" stroke-width="2"/>
-        </svg>
-        <p class="Like-Text">${roundedVoteAverage}%</p>
-      </div>
-    `;
-
-        cardSection.innerHTML += cardHTML;
+        cardSection.innerHTML += `
+            <div class="Card-Box">
+                <a href="./pages/detail.html?id=${item.id}&type=${mediaType}" class="media-link">
+                    <img src="https://image.tmdb.org/t/p/w500${posterPath}" alt="${title}">
+                    <div class="Detail-Box">
+                        <h2 class="Title">${title}</h2>
+                        <p class="Date">${formattedDate}</p>
+                    </div>
+                    <svg class="Like-Box" xmlns="http://www.w3.org/2000/svg" width="63" height="63" viewBox="0 0 63 63" fill="none">
+                        <circle cx="31.5" cy="31.5" r="30.5" fill="#032541" stroke="white" stroke-width="2"/>
+                    </svg>
+                    <p class="Like-Text">${voteAverage}%</p>
+                </a>
+            </div>
+        `;
     });
 }
-// Fonction pour récupérer les tendances du jour
-function fetchDailyTrending() {
-    fetch('https://api.themoviedb.org/3/trending/movie/day?language=fr-FR', {
+
+function fetchData(url, sectionId, type) {
+    fetch(url, {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${accessToken}`,
             'Content-Type': 'application/json'
         }
     })
-        .then(response => response.json())
-        .then(data => updateCardSection(data.results.slice(0, 4), 'tendance'))
-        .catch(error => console.error('Erreur lors de la récupération des tendances du jour:', error));
+    .then(response => response.json())
+    .then(data => updateCardSection(data.results.slice(0, 4), sectionId, type))
+    .catch(error => console.error(`Erreur lors de la récupération des données:`, error));
 }
-
-// Fonction pour récupérer les tendances de la semaine
-function fetchWeeklyTrending() {
-    fetch('https://api.themoviedb.org/3/trending/movie/week?language=fr-FR', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(data => updateCardSection(data.results.slice(0, 4), 'tendance'))
-        .catch(error => console.error('Erreur lors de la récupération des tendances de la semaine:', error));
-}
-
-// Fonction pour récupérer les séries les mieux notées
-function fetchTopRatedTVShows() {
-    fetch('https://api.themoviedb.org/3/tv/top_rated?language=fr-FR', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(data => updateCardSection(data.results.slice(0, 4), 'Series'))
-        .catch(error => console.error('Erreur lors de la récupération des séries les mieux notées:', error));
-}
-
-// Fonction pour récupérer les séries les plus populaires
-function fetchPopularTVShows() {
-    fetch('https://api.themoviedb.org/3/tv/popular?language=fr-FR', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(data => updateCardSection(data.results.slice(0, 4), 'Series'))
-        .catch(error => console.error('Erreur lors de la récupération des séries les plus populaires:', error));
-}
-
-
-// Exemple d'appel de fonction
-fetchDailyTrending(); //
-fetchTopRatedTVShows(); //
 
 const dailyTrendingButton = document.querySelector('#td');
 const weeklyTrendingButton = document.querySelector('#week');
@@ -106,23 +112,28 @@ const topRatedTVShowsButton = document.querySelector('#top');
 const popularTVShowsButton = document.querySelector('#pop');
 
 dailyTrendingButton.addEventListener('click', () => {
-    fetchDailyTrending();
+    fetchData('https://api.themoviedb.org/3/trending/all/day?language=fr-FR', 'tendance', 'movie');
     dailyTrendingButton.classList.add('active');
     weeklyTrendingButton.classList.remove('active');
 });
-weeklyTrendingButton.addEventListener('click', () => {;
-    fetchWeeklyTrending();
+
+weeklyTrendingButton.addEventListener('click', () => {
+    fetchData('https://api.themoviedb.org/3/trending/all/week?language=fr-FR', 'tendance', 'movie');
     weeklyTrendingButton.classList.add('active');
     dailyTrendingButton.classList.remove('active');
 });
 
 topRatedTVShowsButton.addEventListener('click', () => {
-    fetchTopRatedTVShows();
+    fetchData('https://api.themoviedb.org/3/tv/top_rated?language=fr-FR', 'Series', 'tv');
     topRatedTVShowsButton.classList.add('active');
     popularTVShowsButton.classList.remove('active');
 });
-popularTVShowsButton.addEventListener('click', () => {;
-    fetchPopularTVShows();
+
+popularTVShowsButton.addEventListener('click', () => {
+    fetchData('https://api.themoviedb.org/3/tv/popular?language=fr-FR', 'Series', 'tv');
     popularTVShowsButton.classList.add('active');
     topRatedTVShowsButton.classList.remove('active');
 });
+
+fetchData('https://api.themoviedb.org/3/trending/all/day?language=fr-FR', 'tendance', 'movie');
+fetchData('https://api.themoviedb.org/3/tv/top_rated?language=fr-FR', 'Series', 'tv');
